280bde4d72a9e5f1f4fa2521f2d36f4cdf782689
andrewbituin <fandrewbituin@gmail.com>
2019-03-06 11:56:26 -0800

    finished functionality and testing

--- thingful-server/src/auth/auth-service.js
+++ thingful-server/src/auth/auth-service.js
@@ -21,6 +21,11 @@ const AuthService = {
       subject,
       algorithm: "HS256"
     });
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ["HS256"]
+    });
   }
 };
 
